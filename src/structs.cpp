304ac9b92546ecadcde7418762bd6f3dd7a84853
Joro Georgiev <georgiggeorgiev@tu-sofia.bg>
2024-11-05 16:18:06 +0200

    format structs.cpp

--- src/structs.cpp
+++ src/structs.cpp
@@ -3,25 +3,31 @@
 namespace cg
 {
 
-const Version version = {
+const Version version =
+{
     .gl_major = 4,
     .gl_minor = 6,
-    .glsl_version = "#version 460"};
+    .glsl_version = "#version 460"
+};
 
-const Window window = {
+const Window window =
+{
     .window_width = 1280,
     .window_height = 720,
-    .window_title = "Computer Graphics"};
+    .window_title = "Computer Graphics"
+};
 
 /*
  * Projection.
  * FOV, Aspect, Z_NEAR, Z_FAR
  */
-Perspective perspective = {
+Perspective perspective =
+{
     .fov = 45.0f,
     .aspect = static_cast<float>(window.window_width) / window.window_height,
     .z_near = 1.0f,
-    .z_far = 100.0f};
+    .z_far = 100.0f
+};
 
 /*
  * View.
@@ -29,10 +35,12 @@ Perspective perspective = {
  * Center at 0.
  * Up = y direction.
  */
-Camera camera = {
+Camera camera =
+{
     .eye = glm::vec3(0.0f, 0.0f, 5.0f),
     .center = glm::vec3(0.0f),
-    .up = glm::vec3(0.0f, 1.0f, 0.0f)};
+    .up = glm::vec3(0.0f, 1.0f, 0.0f)
+};
 
 } // namespace cg
 
